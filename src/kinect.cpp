e5bfa112bc395b20296b36732caa218e5b49ef32
Jacob Brunson <jacob@brunson.me>
2015-02-02 17:52:57 -0600

    added support for libfreenect2

---
+++ src/kinect.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <signal.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <libfreenect2/libfreenect2.hpp>
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/threading.h>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
